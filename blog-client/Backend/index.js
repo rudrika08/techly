fba020a57ed546c04022ec8dc3bc5822e5494da9
Ritam Vaskar <142218173+Ritam-Vaskar@users.noreply.github.com>
2024-10-05 19:00:07 +0530

    Update index.js

--- blog-client/Backend/index.js
+++ blog-client/Backend/index.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-  origin: ["https://blog-by-you-frontend.vercel.app/"], 
+  origin: process.env.FRONTEND_URL, 
   credentials: true 
 }));
 
