73bf15d32802e9b29e39ecdfa53233ad92228203
Ritam-Vaskar <vaskarananda77@gmail.com>
2024-10-05 20:07:12 +0530

    api

--- blog-client/Frontend/src/helper/ProtectedRouter.jsx
+++ blog-client/Frontend/src/helper/ProtectedRouter.jsx
@@ -3,7 +3,9 @@ import { Navigate } from 'react-router-dom';
 import useLoginCheck from './LoginCheck';
 import BlogPage from './../pages/Blogpage/BlogPage';
 import Login from './../Authentication/Login/Login';
-
+const api = axios.create({
+  baseURL: 'https://blog-by-you.onrender.com/api',
+});
 const ProtectedRoute = ({ children }) => {
   const isLoggedIn = useLoginCheck();
   return isLoggedIn ? <BlogPage /> : <Login />;
