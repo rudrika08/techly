516e3206bef712af4051376411a5d5d598659f1f
Ritam-Vaskar <vaskarananda77@gmail.com>
2024-10-06 20:45:17 +0530

    method

--- blog-client/Backend/index.js
+++ blog-client/Backend/index.js
@@ -36,7 +36,9 @@ app.use(cors({
       callback(new Error('Not allowed by CORS'));
     }
   },
-  credentials: true
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
 // Handle preflight requests
