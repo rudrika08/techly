9bf6b0724908917bf74f8e6188afdd23ec9ce23f
Ritam Vaskar <142218173+Ritam-Vaskar@users.noreply.github.com>
2024-11-27 20:02:07 +0530

    Update index.js

--- blog-client/Frontend/src/common/index.js
+++ blog-client/Frontend/src/common/index.js
@@ -1,48 +1,48 @@
-const backendURI="import.meta.env.VITE_BACKEND_URI";
+const backendURI = import.meta.env.VITE_BACKEND_URI;
 
-const SummaryApi={
-    signup:{
-        url:`${backendURI}/api/signup`,
-        method:"POST"
+console.log('Backend URL:', backendURI);
+
+const SummaryApi = {
+    signup: {
+        url: backendURI + '/api/signup',
+        method: "POST"
     },
-    login:{
-        url:`${backendURI}/api/login`,
-        method:"POST"
+    login: {
+        url: backendURI + '/api/login',
+        method: "POST"
     },
-    current_user:{
-        url:`${backendURI}/api/userdetails`,
-        method:"GET"
+    current_user: {
+        url: backendURI + '/api/userdetails',
+        method: "GET"
+    },
+    logout: {
+        url: backendURI + '/api/logout',
+        method: "GET"
+    },
+    BlogFetch: {
+        url: backendURI + '/api/blog',
+        method: "GET"
+    },
+    BlogCreate: {
+        url: backendURI + '/api/blogCreate',
+        method: "POST"
+    },
+    BlogFetchById: {
+        url: backendURI + '/api/blogFetchById',
+        method: "GET"
+    },
+    BlogDelete: {
+        url: backendURI + '/api/blogDelete',
+        method: "DELETE"
+    },
+    BlogFetchByBlogId: {
+        url: backendURI + '/api/blogFetchByBlogId',
+        method: "POST"
+    },
+    BlogEdit: {
+        url: backendURI + '/api/blogUpdate',
+        method: "PUT"
     }
-    ,
-    logout:{
-        url:`${backendURI}/api/logout`,
-        method:"GET"
-    },
-    BlogFetch:{
-        url:`${backendURI}/api/blog`,
-        method:"GET"
-    },
-    BlogCreate:{
-        url:`${backendURI}/api/blogCreate`,
-        method:"POST"
-    },
-    BlogFetchById:{
-        url:`${backendURI}/api/blogFetchById`,
-        method:"GET"
-    },
-    BlogDelete:{
-        url:`${backendURI}/api/blogDelete`,
-        method:"DELETE"
-    },
-    BlogFetchByBlogId:{
-        url:`${backendURI}/api/blogFetchByBlogId`,
-        method:"POST"
-    },
-    BlogEdit:{
-        url:`${backendURI}/api/blogUpdate`,
-        method:"PUT"
-    }
-}
+};
 
-
-export default SummaryApi
+export default SummaryApi;
